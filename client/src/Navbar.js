04ab5a9cf9b927285f7309ad0f87b6a382191294
Sanghyup Lee <shawnlee@Sanghyups-MacBook-Air.local>
2024-08-28 19:03:32 -0400

    Created navbar

---
+++ client/src/Navbar.js
@@ -0,0 +1,15 @@
+
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1 className="logo" href="#">RareTrades</h1>
+            <ul className="nav-links">
+                <li><a href="#recommendation">Recommendations</a></li>
+                <li><a href="#search">Search</a></li>
+                <li><a href="#ranking">Ranking</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    )
+}
